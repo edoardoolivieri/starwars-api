e3f6ef8eb9c708d5f962776f7ce71cdc2abeb97d
Edoardo Olivieri <diddo.42@hotmail.it>
2019-08-24 08:21:35 +0100

    added styled-component

--- src/components/title.js
+++ src/components/title.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Title extends Component{
   render(){
     return(
-      <div>
+      <div className="d-flex">
         <h1>Star Wars characters</h1>
         <p>search for people</p>
       </div>
