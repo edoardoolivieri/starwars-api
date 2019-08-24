ade4ed89ae3e824ac0e58bd57329ea56044fadd2
Edoardo Olivieri <diddo.42@hotmail.it>
2019-08-24 08:26:27 +0100

    styled title

--- src/components/title.js
+++ src/components/title.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Titlediv = styled.div`
+  display: flex;
+  flex-direction: column;
+  h1{
+    text-align: center;
+  }
+  p{
+    text-align: center;
+  }
+`
 
 class Title extends Component{
   render(){
     return(
-      <div className="d-flex">
+      <Titlediv>
         <h1>Star Wars characters</h1>
-        <p>search for people</p>
-      </div>
+        <p>Search by people (Jocasta Nu, Ratts Tyerell, R4-P17 ...) to get their infos.</p>
+      </Titlediv>
     );
   }
 }
