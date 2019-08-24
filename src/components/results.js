b9b8b75dc9058100e085c75cca3f50ef1a6cc5cc
Edoardo Olivieri <diddo.42@hotmail.it>
2019-08-24 09:57:00 +0100

    read me update

--- src/components/results.js
+++ src/components/results.js
@@ -13,20 +13,16 @@ class Results extends Component{
   render(){
     return(
       <Resultspeople>
-
-        <Card bg="dark" text="white" style={{ width: '18rem' }}>
+        <Card border="dark">
             <Card.Header>{this.props.name && <h2>{this.props.name}</h2>}</Card.Header>
             <Card.Body>
-              <Card.Text>
-                {this.props.eye_color &&<h5>Eye Color | {this.props.eye_color}</h5>}
-                {this.props.hair_color &&<h5>Hair Color | {this.props.hair_color}</h5>}
-                {this.props.height &&<h5>Height | {this.props.height}cm</h5>}
-                {this.props.skin_color &&<h5>Skin Color | {this.props.skin_color}</h5>}
-                {this.props.error &&<h3>Error! {this.props.error}</h3>}
-              </Card.Text>
+              {this.props.eye_color &&<h5>Eye Color | {this.props.eye_color}</h5>}
+              {this.props.hair_color &&<h5>Hair Color | {this.props.hair_color}</h5>}
+              {this.props.height &&<h5>Height | {this.props.height}cm</h5>}
+              {this.props.skin_color &&<h5>Skin Color | {this.props.skin_color}</h5>}
+              {this.props.error &&<h3>Error! {this.props.error}</h3>}
             </Card.Body>
         </Card>
-
       </Resultspeople>
     );
   }
